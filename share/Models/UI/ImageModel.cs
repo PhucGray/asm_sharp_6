434ff00a8ad40f83a458e84a9114cf4f94d550da
Phúc Nguyễn Trọng <grayfptcs3@gmail.com>
2022-08-07 23:55:33 +0700

    Add project files.

---
+++ share/Models/UI/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace share.Models.UI
+{
+    public class ImageModel
+    {
+        public string Base64data { get; set; }
+        public string ContentType { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ share/Models/UI/SignUpModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace share.Models.UI
+{
+    public class SignUpModel
+    {
+        [Required(ErrorMessage = "Vui lòng nhập họ và tên")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập email")]
+        [EmailAddress(ErrorMessage = "Email không đúng định dạng")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu")]
+        public string Password { get; set; }
+    }
+}
